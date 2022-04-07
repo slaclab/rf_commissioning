5731f654515193b6521fe301e357e50e77390d5e
Sebastian Aderhold <aderhold@slac.stanford.edu>
2022-04-07 15:01:48 -0700

    added gui listeners, reformatted ui file, added table for additional participants

--- GuidedCommissioningScreens.py
+++ GuidedCommissioningScreens.py
@@ -1,7 +1,3 @@
-from typing import Optional
-
-from epics import PV
-from lcls_tools.devices.scLinac import CRYOMODULE_OBJECTS, Cavity, Cryomodule
 from pydm import Display
 
 import utilities as util
@@ -39,33 +35,30 @@ class GuidedCommissioningScreens(Display):
         self.ui.testlead.addItems(util.testlead_list)
         self.ui.testlead.setEnabled(True)
 
-        self.ui.pick_linacsection.addItems(["L0B", "L1B", "L2B", "L3B"])
-        self.ui.pick_linacsection.setEnabled(True)
-
         self.ui.pick_cavity.addItems(util.cavity_list)
         self.ui.pick_cavity.setEnabled(True)
+        self.ui.pick_cavity.currentIndexChanged.connect(self.update_current_cavity_and_cm)
 
         self.ui.pick_radmonitor.addItems(["DecaRad 1", "DecaRad 2"])
         self.ui.pick_radmonitor.setEnabled(True)
+        self.ui.pick_radmonitor.currentIndexChanged.connect(self.update_current_decarad)
 
         self.ui.pick_cm.addItems(util.cryomodule_list)
         self.ui.pick_cm.setEnabled(True)
+        self.ui.pick_cm.currentIndexChanged.connect(self.update_current_cavity_and_cm)
+
+    def update_current_cavity_and_cm(self):
+        self.current_cm = util.COMMISSIONING_CRYOMODULE_OBJECTS[self.ui.pick_cm.currentText()]
+        self.current_cavity = self.current_cm.cavities[int(self.ui.pick_cavity.currentText())]
+
+    def update_current_decarad(self):
+        self.current_decarad = self.ui.pick_radmonitor.currentText()
+        self.ui.button_decaradgui.macros = {"P": "RADM:SYS0:{decarad}00".format(decarad=self.current_decarad),
+                                            "M": self.current_decarad}
 
     def initial_setup(self):
-        # read the initial setup from inputs
-        self.current_cm = self.ui.pick_cm.currentText()
-        self.current_cavity = int(self.ui.pick_cavity.currentText())
-        self.current_decarad = self.ui.pick_radmonitor.currentText()
-        self.current_pvprefix = CRYOMODULE_OBJECTS[self.current_cm].cavities[self.current_cavity].pvPrefix
-        self.ui.label_8.setText(self.current_pvprefix)
-
         # set variables for other tabs
         # Striptool + Interlock tab
-        if self.current_decarad == "DecaRad 1":
-            self.ui.button_decaradgui.macros = {"P": "RADM:SYS0:100", "M": 1}
-
-        else:
-            self.ui.button_decaradgui.macros = {"P": "RADM:SYS0:200", "M": 2}
 
         self.ui.button_interlockoverview.filenames('$TOOLS/edm/display/llrf/rf_srf_intlk_nocryo_embed.edl)')
         # self.ui.button_interlockoverview.macros('C=1,RFS=1A,R=A,CM=ACCL:L1B:02,CH=1,ID=01') #hardcoded set of macros for testing
@@ -105,42 +98,5 @@ class GuidedCommissioningScreens(Display):
         else:
             ch = 1
 
-        macro_string = 'C=' + c + ',RFS=' + rfs + ',R=' + r + ',CM=' + cm + ',CH=' + ch + ',ID=' + id
+        macro_string = "C={c},RFS={rfs},R={r},CM={cm},CH={ch},ID={id}".format(c=c, rfs=rfs, r=r, cm=cm, ch=ch, id=id)
         return macro_string
-
-
-class CommissioningCavity(Cavity):
-    def __init__(self, cavityNum, rackObject):
-        super().__init__(cavityNum, rackObject)
-        self.interlock_pv = PV(self.pvPrefix + "RFPERMIT")
-        self.piezo_prerf_checked: bool = False
-        self.piezo_capacitance_a: Optional[float] = None
-        self.piezo_capacitance_b: Optional[float] = None
-        self.ssa_characterized: bool = False
-        self.is_tuned: bool = False
-        self.cold_landing_frequency: Optional[float] = None
-        self.steps_to_tuned: Optional[int] = None
-        self.eightpiovernine_frequency_measured: bool = False
-        self.cavity_calibration_run: bool = False
-        self.fpc_qext: Optional[float] = None
-        self.probe_qext_measured: bool = False
-        self.probe_qext_value: Optional[float] = None
-        self.piezo_withrf_checked: bool = False
-        self.piezo_amplifiergain_a: Optional[float] = None
-        self.piezo_amplifiergain_b: Optional[float] = None
-        self.piezo_detune_gain: Optional[float] = None
-        self.microphonics_captured: bool = False
-        self.final_phase_offset: Optional[float] = None
-        self.onehourrun_complete: bool = False
-
-    @property
-    def interlocks_cleared(self):
-        return self.interlock_pv.value == 1
-
-
-class CommissioningCryomodule(Cryomodule):
-    def __init__(self, cryoName, linacObject, cavityClass=CommissioningCavity):
-        super().__init__(cryoName, linacObject, CommissioningCavity)
-
-        self.magnet_checked: bool = False
-        self.unit_test_complete: bool = False
--- utilities.py
+++ utilities.py
@@ -1,3 +1,8 @@
+from typing import Optional, List, Dict
+
+from epics import PV
+from lcls_tools.devices.scLinac import Cavity, Cryomodule, Linac, LINAC_TUPLES
+
 testlead_list = [
     'Aderhold, Sebastian',
     'Gonnella, Dan',
@@ -57,3 +62,51 @@ cryomodule_list = [
     '34',
     '35',
 ]
+
+
+class CommissioningCavity(Cavity):
+    def __init__(self, cavityNum, rackObject):
+        super().__init__(cavityNum, rackObject)
+        self.interlock_pv = PV(self.pvPrefix + "RFPERMIT")
+        self.piezo_prerf_checked: bool = False
+        self.piezo_capacitance_a: Optional[float] = None
+        self.piezo_capacitance_b: Optional[float] = None
+        self.ssa_characterized: bool = False
+        self.is_tuned: bool = False
+        self.cold_landing_frequency: Optional[float] = None
+        self.steps_to_tuned: Optional[int] = None
+        self.eightpiovernine_frequency_measured: bool = False
+        self.cavity_calibration_run: bool = False
+        self.fpc_qext: Optional[float] = None
+        self.probe_qext_measured: bool = False
+        self.probe_qext_value: Optional[float] = None
+        self.piezo_withrf_checked: bool = False
+        self.piezo_amplifiergain_a: Optional[float] = None
+        self.piezo_amplifiergain_b: Optional[float] = None
+        self.piezo_detune_gain: Optional[float] = None
+        self.microphonics_captured: bool = False
+        self.final_phase_offset: Optional[float] = None
+        self.onehourrun_complete: bool = False
+
+    @property
+    def interlocks_cleared(self):
+        return self.interlock_pv.value == 1
+
+
+class CommissioningCryomodule(Cryomodule):
+    def __init__(self, cryoName, linacObject, cavityClass=CommissioningCavity):
+        super().__init__(cryoName, linacObject, CommissioningCavity)
+
+        self.magnet_checked: bool = False
+        self.unit_test_complete: bool = False
+
+
+COMMISSIONING_LINAC_OBJECTS: List[Linac] = []
+
+# Utility dictionary to map cryomodule name strings to cryomodule objects
+COMMISSIONING_CRYOMODULE_OBJECTS: Dict[str, CommissioningCryomodule] = {}
+
+for idx, (name, cryomoduleList) in enumerate(LINAC_TUPLES):
+    linac = Linac(name, cryomoduleList, cavityClass=CommissioningCavity, cryomoduleClass=CommissioningCryomodule)
+    COMMISSIONING_LINAC_OBJECTS.append(linac)
+    COMMISSIONING_CRYOMODULE_OBJECTS.update(linac.cryomodules)
