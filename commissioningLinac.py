9edd7bbade6102b4d0e621fb72b7ec9fbf2cf5ee
s-aderhold <68035085+s-aderhold@users.noreply.github.com>
2022-05-03 15:32:51 -0700

    before reorg (#2)
    * added classes CommissioningCavity and CommissioningCryomodule
    
    * this time including all modified files
    
    * updated ui file layout
    
    * added gui listeners, reformatted ui file, added table for additional participants
    
    * got rid of start button, moved initial setup function to init
    
    * changed macro generation for striptools, deleted extra striptool buttons from ui
    
    * initialized current cavity and cm
    
    * fixed Decarad gui button, it works now exclamation mark
    
    * added static macro to interlock button for debugging
    
    * took static macro string for interlock screen out and made it adjust with the current cavity selection
    
    * made function for interlock macro string a property that also will work for other edm screen macros
    
    * modified output of macro_string from list of trings to single string
    
    * fixed macro_string to have the right format for cryomdule identifier
    
    * added space to macro string separation
    
    * added print statement of macro_string for debugging
    
    * changed order of macro string
    
    * sanity check with static interlock macro string
    
    * changed separator in macro string from semicolon to comma
    
    * getting rid of the space in the macro string again
    
    * rebuilt the magnet template repeater, started rf ramp up gui
    
    * named ui elements, added functionality to open new windows
    
    * added magnet reset functionality
    
    * pulled out all buttons from magnet sub-display and connected callbacks
    
    * fixed scLinac import
    
    * fixed edm button bug
    
    * connected more magnet PVs, inverted color scheme of ps status indicator
    
    * added display for live signals and started to connect PVs, not done yet
    
    * added data handling, still need to make cavity numbers ints, not strings
    
    * connect status labels and made them pretty
    
    * added ssa calibration function, including popup screen for error handling
    
    * fixing edm expert button filename
    
    * partially implemented cavity calibration, moved commissioningLinac to its own file
    
    * started to conected buttons for RF controls, finished cavity characterization
    
    * added a bunch of PVs to commissioningLinac, used new waveform plot utility
    
    * added legend to waveform plot, added exception handling for CAseverityError
    
    * implemented 8pi/9 search
    
    * added decarad class, but struggling with amax logic
    
    * added some TODOs, added PVs for piezo check with RF
    
    * connected cryo and vacuum signals to plot and handled radiation
    
    * accomodated changes to use custom ssa classes
    
    * added piezo class, implemented piezo with-rf check and finished piezo pre-rf check
    
    * in the middle of re-org
    
    * fixed imports
    
    * moving magnet stuff out
    
    * fixed more imports, and finished removing magnet class
    
    * added magnet handling
    
    * fixed import statements
    
    * before reorg
    
    Co-authored-by: Lisa Zacarias <zacarias@slac.stanford.edu>

---
+++ commissioningLinac.py
@@ -0,0 +1,249 @@
+from datetime import datetime, timedelta
+from time import sleep
+from typing import Dict, List, Optional, Tuple
+
+from epics import PV
+from numpy import nanmean
+
+import commissioningUtilities as utils
+from lcls_tools.superconducting.scLinac import Cavity, Cryomodule, Magnet, Rack, SSA, make_lcls_cryomodules
+
+
+class DecaradHead:
+    def __init__(self, number, decarad):
+        # type (int, Decarad) -> None
+
+        if number not in range(1, 11):
+            raise AttributeError("Decarad Head number need to be between 1 and 10")
+
+        self.decarad = decarad
+        self.number = number
+
+        # Adds leading 0 to numbers with less than 2 digits
+        self.pvPrefix = self.decarad.pvPrefix + "{:02d}:".format(self.number)
+
+        self.doseRatePV: PV = PV(self.pvPrefix + "GAMMA_DOSE_RATE")
+
+    @property
+    def avgDose(self) -> float:
+        # try to do averaging of the last 60 points to account for signal noise
+        try:
+            archiverData = utils.ARCHIVER.getDataWithTimeInterval(pvList=[self.doseRatePV.pvname],
+                                                                  startTime=(datetime.now() - timedelta(minutes=1)),
+                                                                  endTime=datetime.now(),
+                                                                  timeDelta=timedelta(seconds=1))
+
+            averageDose = nanmean(archiverData.values[self.doseRatePV.pvname])
+
+            return max(averageDose - utils.DECARAD_BACKGROUND_READING, 0)
+
+        # return the most recent value if we can't average for whatever reason
+        except AttributeError:
+            return self.doseRatePV.value
+
+
+class Decarad:
+    def __init__(self, number):
+        # type: (int) -> None
+        if number not in [1, 2]:
+            raise AttributeError("Decarad needs to be 1 or 2")
+        self.number = number
+        self.pvPrefix = "RADM:SYS0:{num}00:".format(num=self.number)
+        self.powerControlPV = PV(self.pvPrefix + "HVCTRL")
+        self.powerStatusPV = PV(self.pvPrefix + "HVSTATUS")
+        self.voltageReadbackPV = PV(self.pvPrefix + "HVMON")
+
+        self.heads = {head: DecaradHead(number=head, decarad=self)
+                      for head in range(1, 11)}
+
+    @property
+    def max_avg_dose(self):
+        return max([head.avgDose for head in self.heads.values()])
+
+
+class Piezo:
+    def __init__(self, cavity):
+        # type (CommissioningCavity) -> None
+        self.cavity: CommissioningCavity = cavity
+        self.pvPrefix: str = self.cavity.pvPrefix + "PZT:"
+
+        self.enable_PV: PV = PV(self.pvPrefix + "ENABLE")
+        self.feedback_mode_PV: PV = PV(self.pvPrefix + "MODECTRL")
+        self.dc_setpoint_PV: PV = PV(self.pvPrefix + "DAC_SP")
+        self.prerf_run_check_PV: PV = PV(self.pvPrefix + "TESTSTRT")
+        self.prerf_cha_status_PV: PV = PV(self.pvPrefix + "CHA_TESTSTAT")
+        self.prerf_chb_status_PV: PV = PV(self.pvPrefix + "CHB_TESTSTAT")
+        self.prerf_cha_testmsg_PV: PV = PV(self.pvPrefix + "CHA_TESTMSG1")
+        self.prerf_chb_testmsg_PV: PV = PV(self.pvPrefix + "CHA_TESTMSG2")
+        self.capacitance_a_PV: PV = PV(self.pvPrefix + "CHA_C")
+        self.capacitance_b_PV: PV = PV(self.pvPrefix + "CHB_C")
+        self.prerf_check_status_PV: PV = PV(self.pvPrefix + "TESTSTS")
+
+        self.withrf_run_check_PV: PV = PV(self.pvPrefix + "RFSTART")
+        self.withrf_check_status_PV: PV = PV(self.pvPrefix + "RFTESTS")
+        self.withrf_status_PV: PV = PV(self.pvPrefix + "RFSTESTSTAT")
+        self.amplifiergain_a_PV: PV = PV(self.pvPrefix + "CHA_AMPGAIN")
+        self.amplifiergain_b_PV: PV = PV(self.pvPrefix + "CHB_AMPGAIN")
+        self.withrf_push_dfgain_PV: PV = PV(self.pvPrefix + "PUSH_DFGAIN.PROC")
+        self.withrf_save_dfgain_PV: PV = PV(self.pvPrefix + "SAVE_DFGAIN.PROC")
+        self.detunegain_new_PV: PV = PV(self.pvPrefix + "DFGAIN_NEW")
+
+
+class CommissioningCavity(Cavity):
+    def __init__(self, cavityNum, rackObject, length, ssaClass=SSA):
+        super().__init__(cavityNum, rackObject, length=length)
+
+        self.results = utils.CommissioningCavityResults()
+
+        self.piezo = Piezo(self)
+
+        self.interlock_PV: PV = PV(self.pvPrefix + "RFPERMIT")
+        self.coupler_top_PV: PV = PV(self.pvPrefix + "CPLRTEMP1")
+        self.coupler_bot_PV: PV = PV(self.pvPrefix + "CPLRTEMP2")
+        self.hom_us_PV: PV = PV("CTE:CM{cm}:1{cavity}18:UH:TEMP".format(cm=self.cryomodule.name, cavity=self.number))
+        self.hom_ds_PV: PV = PV("CTE:CM{cm}:1{cavity}20:DH:TEMP".format(cm=self.cryomodule.name, cavity=self.number))
+        self.detune_best_PV: PV = PV(self.pvPrefix + "DFBEST")
+        self.detune_rfs_PV: PV = PV(self.pvPrefix + "DF")
+
+        self.ssa_maxdrive_PV: PV = PV(self.pvPrefix + "SSA:DRV_MAX_REQ")
+
+        self.measured_probe_qext_PV: PV = PV(self.pvPrefix + "QPROBE_CALC2")
+        self.inuse_probe_qext_PV: PV = PV(self.pvPrefix + "QPROBE")
+        self.calculate_probe_qext_PV: PV = PV(self.pvPrefix + "QPROBE_CALC1.PROC")
+        self.push_probe_qext_PV: PV = PV(self.pvPrefix + "PUSH_QPROBECALC.PROC")
+
+        self.waveformplot_channelpairs: List[Tuple[Optional[str], str]] = [(None, self.revWaveformPV.pvname),
+                                                                           (None, self.fwdWaveformPV.pvname),
+                                                                           (None, self.cavWaveformPV.pvname)]
+
+        self.acceptancetest_max_amplitude_PV: PV = PV(self.pvPrefix + "AT:AMAX")
+        self.acceptancetest_useable_amplitude_PV: PV = PV(self.pvPrefix + "AT:AUSE")
+        self.acceptancetest_fe_onset_PV: PV = PV(self.pvPrefix + "AT:FEON_AACT")
+        self.acceptancetest_cavity_limitation_PV: PV = PV(self.pvPrefix + "AT:LIMIT")
+
+        self.sel_phaseoffset_PV: PV = PV(self.pvPrefix + "SEL_POFF")
+        self.sel_phaseoffset_rdbk_PV: PV = PV(self.pvPrefix + "SEL_POFF_RBV")
+
+        self.feedback_phase_high_PV: PV = PV(self.pvPrefix + "PHAFB_HSUM")
+        self.feedback_phase_low_PV: PV = PV(self.pvPrefix + "PHAFB_LSUM")
+        self.feedback_amplitude_high_PV: PV = PV(self.pvPrefix + "AMPFB_HSUM")
+        self.feedback_amplitude_low_PV: PV = PV(self.pvPrefix + "AMPFB_LSUM")
+
+        self.freq_search_select_PV: PV = PV(self.pvPrefix + "FSCAN:SEL")
+        self.freq_search_8pi9_PV: PV = PV(self.pvPrefix + "FSCAN:8PI9MODE")
+        self.freq_search_push_PV: PV = PV(self.pvPrefix + "FSCAN:PUSH_8PI9.PROC")
+
+        self.ades_max_srf_PV: PV = PV(self.pvPrefix + "ADES_MAX_SRF")
+
+    def connect_to_decarad(self):
+        for decaradhead in self.cryomodule.decarad.heads.values():
+            decaradhead.doseRatePV.clear_callbacks()
+            decaradhead.doseRatePV.add_callback(self.check_radiation)
+
+    def check_radiation(self):
+        # TODO link ADESMAX puts to GUI
+        if utils.RADIATION_LIMIT > self.cryomodule.decarad.max_avg_dose > 0:
+            threshold = utils.GRADIENT_THRESHOLD_RADLIMIT * self.length
+            self.ades_max_srf_PV.put(min(threshold, self.ades_max_srf_PV.value))
+
+            if self.selAmplitudeActPV.value <= threshold:
+                self.results.max_amplitude = threshold
+                raise utils.RadError('Field emission detected. Proceed with caution without exceeding {thresh} MV.'
+                                     .format(thresh=threshold))
+
+            else:
+                self.results.max_amplitude = self.selAmplitudeDesPV.value
+                raise utils.RadError('Field emission detected above {thresh} MV. Please stop.'
+                                     .format(thresh=threshold))
+
+        elif self.cryomodule.decarad.max_avg_dose >= utils.RADIATION_LIMIT:
+            self.ades_max_srf_PV.put(self.selAmplitudeDesPV.value)
+            raise utils.RadError('Radiation exceeds {limit}mR/hr'.format(limit=utils.RADIATION_LIMIT))
+        else:
+            raise utils.RadError('Negative radiation values detected. Verify that the decarads are reading correctly')
+
+    @property
+    def interlocks_cleared(self) -> bool:
+        return self.interlock_PV.value == 1
+
+    def calculate_probe_q(self):
+        # TODO check if '1' is actually the right thing to put
+        self.calculate_probe_qext_PV.put(1)
+        if utils.PROBE_QEXT_LOWER_LIMIT <= self.measured_probe_qext_PV.value <= utils.PROBE_QEXT_UPPER_LIMIT:
+            self.push_probe_qext_PV.put(1)
+            self.results.probe_qext_value = self.measured_probe_qext_PV.value
+            self.results.probe_qext_measured = True
+        else:
+            raise utils.ProbeQError('Measured probe Q value out of tolerance')
+
+
+class CommissioningRack(Rack):
+    def __init__(self, rackName, cryoObject, cavityClass, cavityLength, ssaClass=SSA):
+        super().__init__(rackName=rackName, cryoObject=cryoObject, cavityClass=CommissioningCavity,
+                         cavityLength=cavityLength)
+
+        self.freq_search_low_PV: PV = PV(self.pvPrefix + "FSCAN:FREQ_START")
+        self.freq_search_high_PV: PV = PV(self.pvPrefix + "FSCAN:FREQ_STOP")
+        self.freq_search_rms_thresh_PV: PV = PV(self.pvPrefix + "FSCAN:RMS_THRESH")
+        self.freq_search_modeoverlap_PV: PV = PV(self.pvPrefix + "FSCAN:MODE_OVERLAP")
+        self.freq_search_start_PV: PV = PV(self.pvPrefix + "FSCAN:START")
+        self.freq_search_status_PV: PV = PV(self.pvPrefix + "FSCAN:STAT")
+
+
+class CommissioningMagnet(Magnet):
+    def __init__(self, magnettype, cryomodule):
+        super().__init__(magnettype, cryomodule)
+
+    def checkout(self):
+        self.reset()
+        self.turnOn()
+        self.degauss()
+        self.bdes = utils.NOMINAL_BDES
+        sleep(3600)
+        self.bdes = 0
+        self.turnOff()
+
+
+class CommissioningCryomodule(Cryomodule):
+    def __init__(self, cryoName, linacObject, cavityClass, magnetClass, rackClass, isHarmonicLinearizer, ssaClass=SSA):
+        super().__init__(cryoName=cryoName, linacObject=linacObject, cavityClass=CommissioningCavity,
+                         rackClass=CommissioningRack,
+                         isHarmonicLinearizer=isHarmonicLinearizer)
+
+        self.results = utils.CommissioningCryomoduleResults()
+        self.cavity_results = {cavity.number: cavity.results for cavity in self.cavities.values()}
+
+        self.stepper_temp_PVs = []
+        self.coupler_top_PVs = []
+        self.coupler_bot_PVs = []
+        self.hom_us_PVs = []
+        self.hom_ds_PVs = []
+        self.detune_PVs = []
+
+        for cavity in self.cavities.values():
+            self.stepper_temp_PVs.append(cavity.stepper_temp_PV.pvname)
+            self.coupler_top_PVs.append(cavity.coupler_top_PV.pvname)
+            self.coupler_bot_PVs.append(cavity.coupler_bot_PV.pvname)
+            self.hom_us_PVs.append(cavity.hom_us_PV.pvname)
+            self.hom_ds_PVs.append(cavity.hom_ds_PV.pvname)
+            self.detune_PVs.append(cavity.detune_best_PV.pvname)
+
+        self.cryo_signal_PVs = [self.dsLevelPV.pvname, self.usLevelPV.pvname,
+                                self.dsPressurePV.pvname, self.jtValveRdbkPV.pvname]
+
+        self.magnet_name_map: Dict[str, Magnet] = {'Quad': self.quad, 'XCor': self.xcor, 'YCor': self.ycor}
+
+        # To be populated from the GUI
+        self.decarad: Optional[Decarad] = None
+
+    @property
+    def decarad_PVs(self):
+        decarad_PVs = []
+        for decaradhead in self.decarad.heads.values():
+            decarad_PVs.append(decaradhead.doseRatePV.pvname)
+        return decarad_PVs
+
+
+COMMISSIONING_CRYOMODULE_OBJECTS: Dict[str, CommissioningCryomodule] = make_lcls_cryomodules(
+    cryomoduleClass=CommissioningCryomodule,
+    rackClass=CommissioningRack, cavityClass=CommissioningCavity, magnetClass=CommissioningMagnet)
---
+++ commissioningUtilities.py
@@ -0,0 +1,107 @@
+import dataclasses
+from typing import Optional
+
+from lcls_tools.common.data_analysis.archiver import Archiver
+
+TESTLEAD_LIST = [
+    'Aderhold, Sebastian',
+    'Gonnella, Dan',
+    'Maniscalco, James',
+    'Nelson, Janice',
+    'Porter, Ryan',
+    'Zacarias, Lisa',
+]
+
+# TODO convert to IDES of 20A
+NOMINAL_BDES = 8.5
+
+RADIATION_LIMIT = 50
+GRADIENT_THRESHOLD_RADLIMIT = 16
+# this value is based on historical data, when the decarads were on, but not seeing any FE from a cavity
+DECARAD_BACKGROUND_READING = 4
+
+PROBE_QEXT_UPPER_LIMIT = 3e12
+PROBE_QEXT_LOWER_LIMIT = 1e11
+
+DECARAD_ON_VALUE = 0
+DECARAD_OFF_VALUE = 1
+
+PIEZO_ENABLE_VALUE = 1
+PIEZO_DISABLE_VALUE = 0
+PIEZO_MANUAL_VALUE = 0
+PIEZO_SCRIPT_RUNNING_VALUE = 2
+PIEZO_SCRIPT_COMPLETE_VALUE = 1
+PIEZO_PRERF_CHECKOUT_STATUS_VALUE = 1
+
+ARCHIVER = Archiver("lcls")
+
+
+class ProbeQError(Exception):
+    """
+    Exception thrown during cavity probe Q calculation
+    """
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class FreqSearchError(Exception):
+    """
+    Exception thrown during 8pi/9 frequency search
+    """
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class RadError(Exception):
+    """
+    Exception thrown during SELAP ramp up
+    """
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class PiezoError(Exception):
+    """
+    Exception thrown piezo checks
+    """
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+@dataclasses.dataclass
+class CommissioningCavityResults:
+    piezo_prerf_checked: bool = False
+    piezo_capacitance_a: Optional[float] = None
+    piezo_capacitance_b: Optional[float] = None
+    ssa_maxdrive: Optional[float] = None
+    ssa_characterized: bool = False
+    is_tuned: bool = False
+    cold_landing_frequency: Optional[float] = None
+    steps_to_tuned: Optional[int] = None
+    eightpiovernine_frequency_measured: bool = False
+    cavity_calibration_run: bool = False
+    fpc_qext: Optional[float] = None
+    probe_qext_measured: bool = False
+    probe_qext_value: Optional[float] = None
+    piezo_withrf_checked: bool = False
+    piezo_amplifiergain_a: Optional[float] = None
+    piezo_amplifiergain_b: Optional[float] = None
+    piezo_detune_gain: Optional[float] = None
+    microphonics_captured: bool = False
+    final_phase_offset: Optional[float] = None
+    onehourrun_complete: bool = False
+    max_amplitude: Optional[float] = None
+
+
+@dataclasses.dataclass
+class CommissioningCryomoduleResults:
+    magnet_checked: bool = False
+    unit_test_complete: bool = False
