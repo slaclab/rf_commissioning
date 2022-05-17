1d8c02651d1b6bba61dff8b1f13686b401732bc1
Lisa Zacarias <zacarias@slac.stanford.edu>
2022-05-16 20:24:43 -0700

    trying to fix radiation popup crashing the OPI

--- cavity_commissioning/commissioning_gui.py
+++ cavity_commissioning/commissioning_gui.py
@@ -83,7 +83,8 @@ class GuidedCommissioningScreens(Display):
             self.rad_detected_popup = make_info_popup(message)
         else:
             self.rad_detected_popup.setText(message)
-            self.rad_detected_popup.exec()
+            self.rad_detected_popup.show()
+            self.rad_detected_popup.raise_()
 
     @slot(str)
     def handle_success(self, message):
