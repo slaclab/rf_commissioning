36763672ad86b2a594436c30961a7c212cf6d227
Lisa Zacarias <zacarias@slac.stanford.edu>
2022-05-19 17:13:44 -0700

    trying to wait for thread before restarting it

--- cavity_commissioning/commissioning_gui.py
+++ cavity_commissioning/commissioning_gui.py
@@ -168,7 +168,7 @@ class GuidedCommissioningScreens(Display):
 
     def restart_thread(self, thread):
         if thread and not thread.isFinished():
-            thread.quit()
+            thread.wait()
             self.ui.status_label.setText("Restarting thread")
 
     def handle_cav_cal_error(self, e):
