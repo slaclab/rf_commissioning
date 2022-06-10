3e696c8b993b0dd062005dd01d97a2a3024c631f
lisazacarias <zacarias@slac.stanford.edu>
2022-06-10 16:27:20 -0700

    minor bug patches, still need to fix stepper moving when GUI loses focus and corrent step count record (#36)

--- cavity_commissioning/commissioning_gui.py
+++ cavity_commissioning/commissioning_gui.py
@@ -334,7 +334,8 @@ class GuidedCommissioningScreens(Display):
         self.tuner_window.ui.button_replace.clicked.connect(self.replace_button_clicked)
         self.tuner_window.ui.button_add.clicked.connect(self.add_button_clicked)
         self.tuner_window.ui.button_mark_tuned.clicked.connect(self.mark_tuned_button_clicked)
-        self.tuner_window.ui.step_des_spinBox.editingFinished.connect(self.launch_stepper_worker)
+        self.tuner_window.ui.step_des_spinBox.setKeyboardTracking(False)
+        self.tuner_window.ui.step_des_spinBox.valueChanged.connect(self.launch_stepper_worker)
     
     def update_plot_timespan(self):
         self.time_plot_updater.updateTimespans(self.live_signals_window.ui.timespan_spinbox.value())
